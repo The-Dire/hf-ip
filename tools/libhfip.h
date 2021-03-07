1105ae0eff0a1e4536efa729cac13885598de24b
helintongh <agh6399@gmail.com>
2021-03-07 13:30:07 +0800

    修改

--- tools/libhfip.h
+++ tools/libhfip.h
@@ -7,33 +7,33 @@
 #include "utils.h"
 
 #ifdef DEBUG_API
-#define lvl_dbg(msg, ...)                                               \
+#define hf_dbg(msg, ...)                                               \
     do {                                                                \
-        print_debug("lvlip ttid %lu "msg, pthread_self(), ##__VA_ARGS__); \
+        print_debug("hfip ttid %lu "msg, pthread_self(), ##__VA_ARGS__); \
     } while (0)
-#define lvl_sock_dbg(msg, sock, ...)                                        \
+#define hf_sock_dbg(msg, sock, ...)                                        \
     do {                                                                \
-        lvl_dbg("lvlfd %d fd %d: "msg, sock->lvlfd, sock->fd, ##__VA_ARGS__); \
+        hf_dbg("hffd %d fd %d: "msg, sock->hffd, sock->fd, ##__VA_ARGS__); \
     } while (0)
 #else
-#define lvl_sock_dbg(msg, sock, ...)
-#define lvl_dbg(msg, ...)
+#define hf_sock_dbg(msg, sock, ...)
+#define hf_dbg(msg, ...)
 #endif
 
-struct lvlip_sock {
+struct hfip_sock {
     struct list_head list;
-    int lvlfd; /* For Level-IP IPC */
+    int hffd; /* For Level-IP IPC */
     int fd;
 };
 
-static inline struct lvlip_sock *lvlip_alloc() {
-    struct lvlip_sock *sock = malloc(sizeof(struct lvlip_sock));
-    memset(sock, 0, sizeof(struct lvlip_sock));
+static inline struct hfip_sock *hfip_alloc() {
+    struct hfip_sock *sock = malloc(sizeof(struct hfip_sock));
+    memset(sock, 0, sizeof(struct hfip_sock));
 
     return sock;
 };
 
-static inline void lvlip_free(struct lvlip_sock *sock) {
+static inline void hfip_free(struct hfip_sock *sock) {
     free(sock);
 }
 
